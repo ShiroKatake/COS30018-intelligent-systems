56ff2cc878e3a272f5207f9f4997bee34c876f8d
ShiroKatake <tgkhang262@gmail.com>
2023-10-10 15:30:48 +1100

    fix(model): changed activation function to fit regression problem

--- train.py
+++ train.py
@@ -93,7 +93,7 @@ def main(argv):
 
     if args.model == 'my_model':
         X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1]))
-        m = model.get_my_model([3, 64, 64, 1])
+        m = model.get_nn([3, 64, 64, 1])
         train_model(m, X_train, y_train, args.model, config)
     if args.model == 'lstm':
         X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
