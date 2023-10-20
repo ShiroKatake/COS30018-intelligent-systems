bf3c59713403a808825b1b345f2836e6a912ad86
ShiroKatake <tgkhang262@gmail.com>
2023-10-20 15:03:34 +1100

    feat(a star): added function to convert flow to speed

--- map/map.py
+++ map/map.py
@@ -11,3 +11,15 @@ class Node:
 
     def __eq__(self, other):
         return self.data == other.data
+
+# Developed by previous COS30018 students (Coulter, Burns, and Henkel)
+def flow_to_speed(flow):
+    A = -1000/32**2     # We assume that the speed at capacity is 32 km/h
+    B = -2 * 32 * A     # and the traffic flow at capacity is 1000 vehicles/hour/lane
+
+    # We also assume that the traffic will always be under capacity
+    # due to lack of sufficient data confirming whether the traffic was under or over capacity,
+    # and most of the day the traffic is assumed to be under capacity
+    speed = min((-B - math.sqrt(B**2 - 4 * A * float(flow))) / 2 * A, 60) # The speed limit in this area is 60 km/h
+
+    return speed
