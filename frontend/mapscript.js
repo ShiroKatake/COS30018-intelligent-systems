1703dd24e5c641b142885958a349d44213fa9392
jess-rubino <102111467@student.swin.edu.au>
2023-10-24 23:51:25 +1100

    feat: added changes to test

--- frontend/mapscript.js
+++ frontend/mapscript.js
@@ -1,73 +1,468 @@
-
 // Initialize the map
-var map = L.map('map').setView([-37.8136, 144.9631], 11);
+var map = L.map("map").setView([-37.8136, 144.9631], 11);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
-
-// function findFastestPath(startPoint, endPoint) {
-//   // Mock data: Simplified path with lat/lng coordinates
-//   var paths = {
-//     '970': { lat: -37.86703, lng: 145.09159 },
-//     '2000': { lat: -37.8516827, lng: 145.0943457 },
-//     '2200': { lat: -37.81631, lng: 145.09812 },
-//   };  
-
-//   var startPointCoords = paths[startPoint];
-//   var endPointCoords = paths[endPoint];
-
-//   if (!startPointCoords || !endPointCoords) {
-//       alert('Invalid start or end point selection.');
-//       return [];
-//   }
-//   return [startPointCoords, endPointCoords];
-// }
-
-var currentRoute;
 
 document.getElementById("form").addEventListener("submit", async function () {
   event.preventDefault();
 
-  var startPoint = document.getElementById("start_scat").value;
-  var endPoint = document.getElementById("end_scat").value;
-  var selectedModel = document.getElementById("model").value;
-  var selectedTime = document.getElementById("time").value;
-  var selectedDate = document.getElementById("date").value;
+  // var startPoint = document.getElementById("start_scat").value;
+  // var endPoint = document.getElementById("end_scat").value;
+  // var selectedModel = document.getElementById("model").value;
+  // var selectedTime = document.getElementById("time").value;
+  // var selectedDate = document.getElementById("date").value;
 
-  const params = {
-    'start_scat': startPoint,
-    'end_scat': endPoint,
-    'model': selectedModel,
-    'time': selectedTime,
-    'date': selectedDate
-  }
-  
-  const url = 'http://localhost:3001';
-  
-  const {data} = await axios.post(url, null, {params})
-    .then(response => {
-      console.log('Response:', response.data);
-    })
-    .catch(error => {
-      console.error('Error:', error);
+  // const params = {
+  //   start_scat: startPoint,
+  //   end_scat: endPoint,
+  //   model: selectedModel,
+  //   time: selectedTime,
+  //   date: selectedDate,
+  // };
+
+  // const url = "http://localhost:3001";
+
+  // const { data } = await axios
+  //   .post(url, null, { params })
+  //   .then((response) => {
+  //     console.log("Response:", response.data);
+  //   })
+  //   .catch((error) => {
+  //     console.error("Error:", error);
+  //   });
+
+  // if (currentRoute) {
+  //   map.removeLayer(currentRoute);
+  // }
+
+  // const routeInfo = data;
+
+  //TODO: return lat long of the first route return red
+  //TODO: return the others as grey
+  const routeInfo = [
+    {
+      route: [
+        {
+          970: {
+            name: "WARRIGAL RD/HIGH STREET RD",
+            lat: -37.86703,
+            long: 145.09159,
+          },
+        },
+        {
+          3685: {
+            name: "WARRIGAL RD/HIGHBURY RD",
+            lat: -37.85467,
+            long: 145.09384,
+          },
+        },
+        {
+          2000: {
+            name: "WARRIGAL RD/TOORAK RD",
+            lat: -37.8516827,
+            long: 145.0943457,
+          },
+        },
+        {
+          4043: {
+            name: "BURKE RD/TOORAK RD",
+            lat: -37.84683,
+            long: 145.05275,
+          },
+        },
+        {
+          4040: {
+            name: "BURKE RD/RIVERSDALE RD",
+            lat: -37.83256,
+            long: 145.05545,
+          },
+        },
+        {
+          4266: {
+            name: "BURWOOD RD/AUBURN RD",
+            lat: -37.82529,
+            long: 145.04387,
+          },
+        },
+        {
+          4264: {
+            name: "GLENFERRIE RD/BURWOOD RD",
+            lat: -37.82389,
+            long: 145.03409,
+          },
+        },
+        {
+          4324: {
+            name: "COTHAM RD/GLENFERRIE RD",
+            lat: -37.809274,
+            long: 145.037306,
+          },
+        },
+        {
+          3662: {
+            name: "PRINCESS ST/HIGH ST",
+            lat: -37.80876,
+            long: 145.02757,
+          },
+        },
+        {
+          2820: {
+            name: "EARL ST/PRINCESS ST",
+            lat: -37.79477,
+            long: 145.03077,
+          },
+        },
+      ],
+      travel_time: 17.781896047690434,
+    },
+    {
+      route: [
+        {
+          970: {
+            name: "WARRIGAL RD/HIGH STREET RD",
+            lat: -37.86703,
+            long: 145.09159,
+          },
+        },
+        {
+          3685: {
+            name: "WARRIGAL RD/HIGHBURY RD",
+            lat: -37.85467,
+            long: 145.09384,
+          },
+        },
+        {
+          2000: {
+            name: "WARRIGAL RD/TOORAK RD",
+            lat: -37.8516827,
+            long: 145.0943457,
+          },
+        },
+        {
+          4043: {
+            name: "BURKE RD/TOORAK RD",
+            lat: -37.84683,
+            long: 145.05275,
+          },
+        },
+        {
+          4040: {
+            name: "BURKE RD/RIVERSDALE RD",
+            lat: -37.83256,
+            long: 145.05545,
+          },
+        },
+        {
+          4266: {
+            name: "BURWOOD RD/AUBURN RD",
+            lat: -37.82529,
+            long: 145.04387,
+          },
+        },
+        {
+          4264: {
+            name: "GLENFERRIE RD/BURWOOD RD",
+            lat: -37.82389,
+            long: 145.03409,
+          },
+        },
+        {
+          4324: {
+            name: "COTHAM RD/GLENFERRIE RD",
+            lat: -37.809274,
+            long: 145.037306,
+          },
+        },
+        {
+          3662: {
+            name: "PRINCESS ST/HIGH ST",
+            lat: -37.80876,
+            long: 145.02757,
+          },
+        },
+        {
+          4335: {
+            name: "HIGH ST/CHARLES ST",
+            lat: -37.80624,
+            long: 145.03518,
+          },
+        },
+        {
+          4321: {
+            name: "HIGH ST/HARP ST",
+            lat: -37.800776,
+            long: 145.0494611,
+          },
+        },
+        {
+          2820: {
+            name: "EARL ST/PRINCESS ST",
+            lat: -37.79477,
+            long: 145.03077,
+          },
+        },
+      ],
+      travel_time: 21.102222433368446,
+    },
+    {
+      route: [
+        {
+          970: {
+            name: "WARRIGAL RD/HIGH STREET RD",
+            lat: -37.86703,
+            long: 145.09159,
+          },
+        },
+        {
+          3685: {
+            name: "WARRIGAL RD/HIGHBURY RD",
+            lat: -37.85467,
+            long: 145.09384,
+          },
+        },
+        {
+          2000: {
+            name: "WARRIGAL RD/TOORAK RD",
+            lat: -37.8516827,
+            long: 145.0943457,
+          },
+        },
+        {
+          4043: {
+            name: "BURKE RD/TOORAK RD",
+            lat: -37.84683,
+            long: 145.05275,
+          },
+        },
+        {
+          4040: {
+            name: "BURKE RD/RIVERSDALE RD",
+            lat: -37.83256,
+            long: 145.05545,
+          },
+        },
+        {
+          4266: {
+            name: "BURWOOD RD/AUBURN RD",
+            lat: -37.82529,
+            long: 145.04387,
+          },
+        },
+        {
+          4264: {
+            name: "GLENFERRIE RD/BURWOOD RD",
+            lat: -37.82389,
+            long: 145.03409,
+          },
+        },
+        {
+          4324: {
+            name: "COTHAM RD/GLENFERRIE RD",
+            lat: -37.809274,
+            long: 145.037306,
+          },
+        },
+        {
+          3662: {
+            name: "PRINCESS ST/HIGH ST",
+            lat: -37.80876,
+            long: 145.02757,
+          },
+        },
+        {
+          4335: {
+            name: "HIGH ST/CHARLES ST",
+            lat: -37.80624,
+            long: 145.03518,
+          },
+        },
+        {
+          4321: {
+            name: "HIGH ST/HARP ST",
+            lat: -37.800776,
+            long: 145.0494611,
+          },
+        },
+        {
+          4030: {
+            name: "BURKE RD/DONCASTER RD",
+            lat: -37.79561,
+            long: 145.06251,
+          },
+        },
+        {
+          2825: {
+            name: "BURKE RD/EASTERN_FWY",
+            lat: -37.78661,
+            long: 145.06202,
+          },
+        },
+        {
+          2820: {
+            name: "EARL ST/PRINCESS ST",
+            lat: -37.79477,
+            long: 145.03077,
+          },
+        },
+      ],
+      travel_time: 25.508124065439738,
+    },
+    {
+      route: [
+        {
+          970: {
+            name: "WARRIGAL RD/HIGH STREET RD",
+            lat: -37.86703,
+            long: 145.09159,
+          },
+        },
+        {
+          3685: {
+            name: "WARRIGAL RD/HIGHBURY RD",
+            lat: -37.85467,
+            long: 145.09384,
+          },
+        },
+        {
+          2000: {
+            name: "WARRIGAL RD/TOORAK RD",
+            lat: -37.8516827,
+            long: 145.0943457,
+          },
+        },
+        {
+          4043: {
+            name: "BURKE RD/TOORAK RD",
+            lat: -37.84683,
+            long: 145.05275,
+          },
+        },
+        {
+          4040: {
+            name: "BURKE RD/RIVERSDALE RD",
+            lat: -37.83256,
+            long: 145.05545,
+          },
+        },
+        {
+          4266: {
+            name: "BURWOOD RD/AUBURN RD",
+            lat: -37.82529,
+            long: 145.04387,
+          },
+        },
+        {
+          4264: {
+            name: "GLENFERRIE RD/BURWOOD RD",
+            lat: -37.82389,
+            long: 145.03409,
+          },
+        },
+        {
+          4324: {
+            name: "COTHAM RD/GLENFERRIE RD",
+            lat: -37.809274,
+            long: 145.037306,
+          },
+        },
+        {
+          3662: {
+            name: "PRINCESS ST/HIGH ST",
+            lat: -37.80876,
+            long: 145.02757,
+          },
+        },
+        {
+          4335: {
+            name: "HIGH ST/CHARLES ST",
+            lat: -37.80624,
+            long: 145.03518,
+          },
+        },
+        {
+          4321: {
+            name: "HIGH ST/HARP ST",
+            lat: -37.800776,
+            long: 145.0494611,
+          },
+        },
+        {
+          4032: {
+            name: "BURKE RD/HARP RD",
+            lat: -37.80202,
+            long: 145.06127,
+          },
+        },
+        {
+          4030: {
+            name: "BURKE RD/DONCASTER RD",
+            lat: -37.79561,
+            long: 145.06251,
+          },
+        },
+        {
+          2825: {
+            name: "BURKE RD/EASTERN_FWY",
+            lat: -37.78661,
+            long: 145.06202,
+          },
+        },
+        {
+          2820: {
+            name: "EARL ST/PRINCESS ST",
+            lat: -37.79477,
+            long: 145.03077,
+          },
+        },
+      ],
+      travel_time: 26.4926930061878,
+    },
+  ];
+
+  // var path = L.polyline(routeInfo, { color: "blue" }).addTo(map);
+
+  // Create a polyline using the coordinates
+  // var path = L.polyline(coordinates, { color: "blue" });
+
+  // Add the polyline to the map
+  // path.addTo(map);
+
+  // routeInfo[0], color: red
+  // routeInfo[1], color: grey
+
+  // Initialize polyline array
+  var polylines = [];
+
+  // Delete old paths
+  polylines.forEach((polyline) => {
+    map.removeLayer(polyline);
   });
-  
-  if (currentRoute) {
-    map.removeLayer(currentRoute);
+
+  // Clear the array
+  polylines = [];
+
+  // Iterate over the routeInfo to return lat and longs of each route
+  routeInfo.forEach((route, index) => {
+    const pathCoordinates = [];
+
+    route.route.forEach((location) => {
+      const [key, value] = Object.entries(location)[0];
+      const lat = value.lat;
+      const lng = value.long;
+      pathCoordinates.push([lat, lng]);
+    });
+
+    const pathColour = index === 0 ? "blue" : "grey";
+    var path = L.polyline(pathCoordinates, { color: pathColour });
+
+    path.addTo(map);
+    polylines.push(path);
+  });
+
+  if (polylines.length > 0) {
+    map.fitBounds(L.featureGroup(polylines).getBounds());
   }
-
-  // TODO: change this to be the model shit 
-  // var fastestPathCoordinates = findFastestPath(startPoint, endPoint);
-
-  const routeInfo = data;
-
-  // TODO: make 4 - 1 blue, 3 grey
-  var path = L.polyline(routeInfo, { color: 'blue' }).addTo(map);
-  // Fit the map to the bounds of the path
-  map.fitBounds(path.getBounds());
-
-  currentRoute = path;
 });
-
-
